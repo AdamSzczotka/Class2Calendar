0cfbfd25d516d976ddef298d44f7461608cf16b8
Adam Szczotka <114624941+AdamSzczotka@users.noreply.github.com>
2025-02-08 12:53:42 +0100

    feat: add get_calendar_service() function

---
+++ Class2Calendar.py
@@ -0,0 +1,45 @@
+import os
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import pickle
+from datetime import datetime, timedelta
+import requests
+from bs4 import BeautifulSoup
+
+# Zakres uprawnień dla Google Calendar
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+
+def get_calendar_service():
+    """Autoryzacja i utworzenie usługi Google Calendar."""
+    creds = None
+
+    # Sprawdzenie czy istnieje zapisany token
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    # Jeśli brak poświadczeń lub są nieważne
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Zapisanie poświadczeń do wykorzystania później
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('calendar', 'v3', credentials=creds)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
